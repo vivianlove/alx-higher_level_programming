8bd1cae9b30124a8f898cf8ab936327418dd008f
vivianlove <ezeoruchinenye@gmail.com>
2023-01-25 21:52:35 -0800

    Great

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,6 +1,6 @@
-:wq
 #!/usr/bin/python3
-
+# 101-square.py
+# Brennan D Baraban <375@holbertonschool.com>
 """Define a class Square."""
 
 
@@ -9,7 +9,6 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
-
         Args:
             size (int): The size of the new square.
             position (int, int): The position of the new square.
